78fa39c007ab59725ad5697e31f6d539d09c11b7
Alireza Salehi <46676671+SalehiAlireza@users.noreply.github.com>
2023-05-07 14:56:49 +0330

    add enable pdo_mysql from code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+       $ext = get_loaded_extensions();	
+
+       if (!in_array('pdo_mysql',$ext)) 
+	   {
+		   
+		   if(! ini_get('enable_dl'))
+			   dd('first run enable_dl in php.ini');
+		   
+			dl('pdo_mysql');	
+	   }
     }
 
     /**
